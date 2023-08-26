12a81e32827f50f56a79a4cf68859b357489db50
FlorSousa <jhonatas.flor@dcx.ufpb.br>
2023-08-26 07:38:07 -0300

    Beginning

---
+++ tracker.py
@@ -0,0 +1,11 @@
+# Check the position of mice and if they are in a ROI
+#  Make ROI selection
+#  Count the time that each mouse has spent in each ROI.
+#  Calculate the mouse's speed.
+#  Save the mice's data in a log file
+
+def SelectRoi():
+    pass
+
+if __name__ == "__main__":
+    pass
