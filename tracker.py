c5ab3ac307e1a26d800d2121742c4d071ec3d4dc
jhonatas flor <jhonatas.flor@dcx.ufpb.br>
2023-09-27 04:24:43 -0300

    Usando C e V para alterar valores do threshold

--- tracker.py
+++ tracker.py
@@ -29,10 +29,31 @@ def open_capture():
         print(f"Error: {str(e)}")
         exit()
 
-def thresholding():
-    import random
-    beta = random.randint(50, round(255/2))
-    return (255/2) + beta
+def thresholding(old_threshold=None,mode=None):
+    if mode == None:  
+        import random
+        beta = random.randint(50, round(255/2))
+        return (255/2) + beta
+
+    if mode == 0:
+        old_threshold -= 1
+        return old_threshold
+    
+    if mode == 1:
+        old_threshold += 1
+        return old_threshold
+    
+def getContour(x,y,x2,y2):
+    return np.array([
+        [x,y],
+        [x2,y],
+        [x2,y2],
+        [x,y2]
+    ], dtype=np.int32)
+
+
+def calculate_time(num_frames):
+    return num_frames * 33
 
 def draw_roi(frame):
     if("rois" in project_data.keys()):
@@ -41,15 +62,19 @@ def draw_roi(frame):
             bottom_right = (roi["x"]+roi["w"], roi["y"]+roi["h"])
             mark_color = (128, 244, 66)
             cv.rectangle(frame, top_left, bottom_right,mark_color,2)
-            cv.putText(frame,f'Counter: {rois_counter[index]}', (top_left[0], bottom_right[1]), cv.FONT_HERSHEY_COMPLEX,1, (255,255,255))
-            
+            cv.putText(frame,f'Counter: {(rois_counter[index]["counter"])}', (top_left[0], bottom_right[1]), cv.FONT_HERSHEY_COMPLEX,1, (255,255,255))
+
+            roi_contour = getContour(top_left[0],top_left[1],bottom_right[0], bottom_right[1])
+            in_roi = True if cv.pointPolygonTest(roi_contour,mice_centroid,False) >= 0 else False
+          
+
         return frame
     else:
         print("Error: The project data don't have a ROI list")
         exit()
 
-def select_threshold(static_frame):
-    threshold = thresholding()
+def select_threshold(static_frame,old_threshold=None,mode=None):
+    threshold = thresholding(old_threshold,mode)
     gray_frame = cv.cvtColor(static_frame, cv.COLOR_BGR2GRAY)
     filtered_frame = cv.threshold(gray_frame, threshold, 255, cv.THRESH_BINARY)[1]
     cv.imshow("Select Threshold Value", filtered_frame)
@@ -59,8 +84,10 @@ def select_threshold(static_frame):
     if key == 27 or key == ord('q'):
         cv.destroyAllWindows()
         return threshold
+    elif key == ord('v'):
+        return select_threshold(static_frame,threshold,1)
     elif key == ord('c'):
-        return select_threshold(static_frame)
+        return select_threshold(static_frame, threshold,0)
 
 def major_contour(contours):
     major =  None
@@ -72,11 +99,22 @@ def major_contour(contours):
 
     return major
 
+def draw_centroid():
+    area = cv.moments(mice_contour)
+    if area["m00"]:
+        cx = int(area["m10"]/area["m00"])
+        cy = int(area["m01"]/area["m00"])
+        cv.circle(frame, (cx,cy), 4, (0,0,255), -1)
+        return (cx,cy)
+    
+    return None
+
 if __name__ == "__main__":
     args = parser_args()
     project_data = open_data()
     rois = project_data["rois"]
-    rois_counter = [ 0 for _ in range(len(rois)) ]
+    p = False
+    rois_counter = [ {"id":i,"counter":0, "frame_counter":0} for i in range(len(rois)) ]
     capture = open_capture()
     ret, bg_frame = capture.read()
     threshold = select_threshold(bg_frame)
@@ -99,12 +137,13 @@ if __name__ == "__main__":
         gray_frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
         filtered_frame = cv.threshold(gray_frame, threshold, 255, cv.THRESH_BINARY)[1]
         contours, _ = cv.findContours(filtered_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_TC89_KCOS)
-        c = major_contour(contours)
-        cv.drawContours(frame, c, -1, (0, 255, 0), 3)
+        mice_contour = major_contour(contours)
+        cv.drawContours(frame, mice_contour, -1, (0, 255, 0), 3)
+        mice_centroid = draw_centroid()
         frame = draw_roi(frame)
         cv.imshow(window_name, frame)
         pbar.update(1)
         frameIndex+=1
         
-        if cv.waitKey(30) & 0xFF == ord('q'):
+        if cv.waitKey(33) & 0xFF == ord('q'):
             break
