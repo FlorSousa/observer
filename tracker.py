2f5c8628ee236add5fa7e5fcde0a19c0c8bfc0ed
jhonatas flor <jhonatas.flor@dcx.ufpb.br>
2023-09-27 09:40:27 -0300

    Contando o tempo total

--- tracker.py
+++ tracker.py
@@ -52,8 +52,12 @@ def getContour(x,y,x2,y2):
     ], dtype=np.int32)
 
 
-def calculate_time(num_frames):
-    return num_frames * 33
+def calculate_time():
+    full_time = 0
+    for roi in rois_counter:
+        full_time += roi["frame_counter"]
+    
+    return full_time
 
 def draw_roi(frame):
     if("rois" in project_data.keys()):
@@ -63,10 +67,21 @@ def draw_roi(frame):
             mark_color = (128, 244, 66)
             cv.rectangle(frame, top_left, bottom_right,mark_color,2)
             cv.putText(frame,f'Counter: {(rois_counter[index]["counter"])}', (top_left[0], bottom_right[1]), cv.FONT_HERSHEY_COMPLEX,1, (255,255,255))
+            cv.putText(frame,f'Time: {(rois_counter[index]["frame_counter"])} ms', (top_left[0], bottom_right[1]-50), cv.FONT_HERSHEY_COMPLEX,1, (255,255,255))
 
             roi_contour = getContour(top_left[0],top_left[1],bottom_right[0], bottom_right[1])
             in_roi = True if cv.pointPolygonTest(roi_contour,mice_centroid,False) >= 0 else False
-          
+            if in_roi:
+                if not rois_counter[index]["in"]:
+                    rois_counter[index]["counter"]+=1
+                    rois_counter[index]["in"] = True
+                rois_counter[index]["frame_counter"]+=33
+                continue
+                
+        
+            if not in_roi:
+                rois_counter[index]["in"] = False
+                
 
         return frame
     else:
@@ -78,7 +93,6 @@ def select_threshold(static_frame,old_threshold=None,mode=None):
     gray_frame = cv.cvtColor(static_frame, cv.COLOR_BGR2GRAY)
     filtered_frame = cv.threshold(gray_frame, threshold, 255, cv.THRESH_BINARY)[1]
     cv.imshow("Select Threshold Value", filtered_frame)
-    
     key = cv.waitKey(0)
     
     if key == 27 or key == ord('q'):
@@ -113,8 +127,7 @@ if __name__ == "__main__":
     args = parser_args()
     project_data = open_data()
     rois = project_data["rois"]
-    p = False
-    rois_counter = [ {"id":i,"counter":0, "frame_counter":0} for i in range(len(rois)) ]
+    rois_counter = [ {"id":i,"counter":0, "frame_counter":0, "in":False} for i in range(len(rois)) ]
     capture = open_capture()
     ret, bg_frame = capture.read()
     threshold = select_threshold(bg_frame)
@@ -127,7 +140,6 @@ if __name__ == "__main__":
     current_pos = (0, 0)
     num_frames = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
     pbar = tqdm(total=num_frames)
-
     while(capture.isOpened()):
         ret,frame = capture.read()
         if not ret:
@@ -144,6 +156,8 @@ if __name__ == "__main__":
         cv.imshow(window_name, frame)
         pbar.update(1)
         frameIndex+=1
-        
-        if cv.waitKey(33) & 0xFF == ord('q'):
+
+        if cv.waitKey(33) & 0xFF == ord('q'):      
             break
+    
+    print(calculate_time())
